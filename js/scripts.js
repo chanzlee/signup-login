54a642e2fc859eef49b84746e6efb43c8732507b
Chan Lee <chanethanlee@gmail.com>
2018-08-21 20:41:52 -0700

    add login field andset to toggle when clink the links vice and versan for sign up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-var accounts = {};
+accounts = {};
 
 var Account = function (id, pw){
   this.id= id;
   this.pw= pw;
 }
 
-
-
 $(document).ready(function(){
   $("#create-account").submit(function(event){
     event.preventDefault();
     var createIdInput = $("#input-create-id").val();
     var createPwInput = $("#input-create-pw").val();
-    console.log(createIdInput + createPwInput);
+    // create new Account object with given inputs
+    var accountInstance = new Account(createIdInput,createPwInput);
+    accounts[accountInstance.id]=accountInstance;
+    $("#create-account")[0].reset();
+  });
+
+  $("#login-account").submit(function(event){
+    event.preventDefault();
+    var idInput = $("#input-create-id").val();
+    var pwInput = $("#input-create-pw").val();
+
+    // credential validation function should be added here
+
+    $("#create-account")[0].reset();
+  });
+
+  $("#login-link").click(function(){
+    $("div.login").toggle();
+    $("div.signup").toggle();
+  });
+
+  $("#signup-link").click(function(){
+    $("div.signup").toggle();
+    $("div.login").toggle();
   });
 });
